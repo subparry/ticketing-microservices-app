c02add3c679f2c2fc7f3bf8eed6b342a8eed699e
Adrian Parry <subparry@gmail.com>
2021-01-27 01:14:38 -0300

    update start msg

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper'
 
 const start = async () => {
-  console.log('Starting orders service...')
+  console.log('Starting orders service....')
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
